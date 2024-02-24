c3b4115f3235e3891918291dd91c417726e29825
fernando fraga <fernandogrfraga@gmail.com>
2024-02-24 14:05:08 -0300

    Construção de um método dentro da classe restaurante para receber os objetos instanciados no modulo de cardapio

--- Formação3/curso-python-2-aula_5/Exercicios/ExCarro/Carro.py
+++ Formação3/curso-python-2-aula_5/Exercicios/ExCarro/Carro.py
@@ -4,6 +4,7 @@ class Carro(Veiculo):
     def __init__(self, marca, modelo, portas, ligado=False) -> None:
         super().__init__(marca, modelo, ligado)
         self.portas = portas
+    
     def __str__(self) -> str:
         return f'{self.marca} | {self.modelo} | {self._ligado} | {self.portas}'
     
